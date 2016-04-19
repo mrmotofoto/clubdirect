dfc442cd48cb8c94ff65bd30f2062c213a54cc9a
moondogg71 <richardstiehm@gmail.com>
2016-04-19 19:37:46 +0000

    Working Basic Page

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
-
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
    res.render('landing'); 
@@ -12,12 +15,12 @@ app.get('/', function(req, res) {
         {
         name: "Coral Ridge Country Club",
         address: "3801 Bayview Dr., Fort Lauderdale, FL 33308",
-        hrContact: "John Smith",
-        hrPhone: "(954) 449-0399",
-        hrEmail: "default@gmail.com",
-        GmContact: "None",
-        GmPhone: "347-452-9552",
-        GmEmail: "gm@club.com",
+        hrcontact: "John Smith",
+        hrphone: "(954) 449-0399",
+        hremail: "default@gmail.com",
+        gmcontact: "None",
+        gmphone: "347-452-9552",
+        gmemail: "gm@club.com",
         distance: "6.7 miles",
         notes: "This could be General Notes Section",
         image: "https://farm3.staticflickr.com/2217/1711688729_10fd998b5d.jpg",
@@ -25,12 +28,12 @@ app.get('/', function(req, res) {
         {
         name: "Fort Lauderdale Country Club",
         address: "415 E Country Club Cir, Fort Lauderdale, FL 33317",
-        hrContact: "Harry Smith",
-        hrPhone: "347-666-1234",
-        hrEmail: "default@gmail.com",
-        GmContact: "Anthony Mandatta",
-        GmPhone: "347-452-9552",
-        GmEmail: "gmmanaer@club.com",
+        hrcontact: "Harry Smith",
+        hrphone: "347-666-1234",
+        hremail: "default@gmail.com",
+        gmcontact: "Anthony Mandatta",
+        gmphone: "347-452-9552",
+        gmemail: "gmmanaer@club.com",
         distance: "12.7 miles",
         notes: "This could be General Notes Section Some more Notes",
         image: "https://farm3.staticflickr.com/2217/1711688729_10fd998b5d.jpg"
@@ -39,12 +42,12 @@ app.get('/', function(req, res) {
         {
         name: "Bonaventure Country Club",
         address: "3201 W Rolling Hills Cir, Fort Lauderdale, FL 33328",
-        hrContact: "Joey Bianchi",
-        hrPhone: "342-234-1234",
-        hrEmail: "defaulthr@gmail.com",
-        GmContact: "Tony Montana",
-        GmPhone: "347-898-0987",
-        GmEmail: "gmmanager@club.com",
+        hrcontact: "Joey Bianchi",
+        hrphone: "342-234-1234",
+        hremail: "defaulthr@gmail.com",
+        gmcontact: "Tony Montana",
+        gmphone: "347-898-0987",
+        gmemail: "gmmanager@club.com",
         distance: "17.7 miles",
         notes: "This could be General Notes Section Some more Notes",
         image: "https://farm9.staticflickr.com/8536/8778590094_60024e6423.jpg"
@@ -52,25 +55,65 @@ app.get('/', function(req, res) {
         {
         name: "Another Country Club",
         address: "324 Ocena Ave, Delray Beach, FL 33308",
-        hrContact: "Joey Jesus",
-        hrPhone: "(954) 456-9087",
-        hrEmail: "joey@gmail.com",
-        GmContact: "None",
-        GmPhone: "347-452-9552",
-        GmEmail: "gm@club.com",
+        hrcontact: "Joey Jesus",
+        hrphone: "(954) 456-9087",
+        hremail: "joey@gmail.com",
+        gmcontact: "None",
+        gmphone: "347-452-9552",
+        gmemail: "gm@club.com",
         distance: "9.7 miles",
         notes: "This could be General Notes Section",
         image: "https://farm1.staticflickr.com/63/187187176_73d89267be.jpg",
         },
         ]
 
+
 app.get('/clubs', function(req, res) {
-        res.render('clubs', {clubs: clubs});
+    res.render('clubs', {clubs: clubs});
+});
+
+app.post('/clubs', function(req, res) {
+var noEntry = "No Entry";
+var defaultImg = "https://farm1.staticflickr.com/63/187187176_73d89267be.jpg"
+  
+var name = req.body.name ? req.body.name : noEntry ,
+    address = req.body.address ? req.body.address : noEntry,
+    hrcontact = req.body.hrcontact ? req.body.hrcontact : noEntry,
+    hrphone = req.body.hrphone ? req.body.hrphone : noEntry,
+    hremail = req.body.hremail ? req.body.hremail : noEntry,
+    gmcontact = req.body.gmcontact ? req.body.gmcontact : noEntry,
+    gmphone = req.body.gmphone ? req.body.gmphone : noEntry,
+    gmemail = req.body.gmemail ? req.body.gmemail : noEntry,
+    distance = req.body.distance ? req.body.distance : noEntry,
+    notes = req.body.notes ? req.body.notes : noEntry,
+    image = req.body.image ? req.body.notes : defaultImg;
+    
+var newClub = {
+    name:name,
+    address: address,
+    hrcontact:hrcontact,
+    hremail:hremail,
+    hrphone:hrphone,
+    gmcontact:gmcontact,
+    gmemail:gmemail,
+    gmphone:gmphone,
+    distance:distance,
+    notes:notes,
+    image:image
+}
+    clubs.push(newClub);
+    res.redirect('/clubs');
+});
+
+app.get('/clubs/new', function(req, res) {
+    res.render('new');
 });
 
 app.get('/show', function(req, res) {
    res.render('show', {clubs: clubs}); 
 });
+
+
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
